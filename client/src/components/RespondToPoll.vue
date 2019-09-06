34b5dd263e3192f322c0ce1c669b0cfb29c1e6f4
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-09-05 18:41:25 -0700

    Added animations, transitions, and new colors

--- client/src/components/RespondToPoll.vue
+++ client/src/components/RespondToPoll.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-100">
-        <div class="card">
+        <div class="card cardbgGray">
             <div class="card-body">
                 <h2 id="prompt">{{ prompt }}</h2>
                 <div id="answerChoices">
-                <button class="btn btn-light btn-lg d-block btn-block selection m-3" v-for="(answer, index) in this.answerChoices" :key="answer" name="answer_choice" :value="index" @click="clicked(index)" :class="[active==index ? 'selectedButton' : 'notSelected']"><i class="fa fa-check-circle fa-2x circleCheck" v-if="active==index"></i> <i class="fa fa-circle-thin fa-2x circleOnly" v-else></i> {{answer}}</button>
+                <button class="btn btn-light btn-lg d-block btn-block selection mb-10 buttonText" v-cloak v-for="(answer, index) in this.answerChoices" :key="answer" name="answer_choice" :value="index" @click="clicked(index)" :class="[active==index ? 'selectedButton moveToRight' : 'notSelected']"><i class="fa fa-check-circle fa-2x circleCheck" v-if="active==index" :class="[active==index ? 'animated zoomIn' : '']"></i> <i class="fa fa-circle-thin fa-2x circleOnly" v-else></i> {{answer}}</button>
                 <!--
                     <form @submit.prevent="submitAnswer">
                         <ul>
@@ -17,7 +17,7 @@
                         </ul>
                         <input type="submit" value="Submit" role="button" class="btn btn-success btn-lg">
                     </form>-->
-                    <button type="submit" class="btn btn-success btn-lg" @click="submitAnswer">Submit <i class="fa fa-check"></i></button>
+                    <button type="submit" class="btn btn-success btn-lg mt-30" @click="submitAnswer">Submit <i class="fa fa-check"></i></button>
                 </div>
             </div>
         </div>
@@ -61,21 +61,49 @@ ul {
     }
 }
 
-.circleOnly,
+.buttonText{
+  text-align: left !important;
+  word-break: normal;
+}
+
+.cardbgGray{
+  background: #f7f7f7 !important;
+}
+
+.circleOnly{
+  color: #E4E4E4;
+}
 .circleCheck{
-  color: #218838;
+  color: #28a745;
+}
+
+.moveToRight{
+  transform: translateX(15px);
+  transition: transform .2s linear;
 }
 
 .selection:hover{
   background: white !important;
-  box-shadow: 3px 4px 4px #ECECEC;
+  box-shadow: 4px 5px 5px #d8d8d8;
+  transform: translateX(15px);
+  transition: transform .2s linear;
 }
 
 .selectedButton, selection:focus{
-   border: 3px solid #218838 !important;
+   border: 3px solid #28a745 !important;
+   box-shadow: 5px 9px 18px #d8d8d8;
+   background: white;
+}
+
+.notSelected:hover{
+  transform: scale(1.02);
+  transition: transform .2s linear;
 }
 
 .notSelected{
    border: 3px solid #ECECEC;
+   transition: transform .3s linear;
+   box-shadow: 4px 5px 5px #d8d8d8;
+   background: white;
 }
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'animate.css/animate.min.css';
 
 Vue.config.productionTip = false;
 
